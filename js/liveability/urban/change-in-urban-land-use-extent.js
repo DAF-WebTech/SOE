2045b24f2a02477f6a21e35b3d8af2c829ea5621
David Porter <david.porter@daf.qld.gov.au>
2020-06-05 10:57:53 +1000

    added closing div that was missing

--- js/liveability/urban/change-in-urban-land-use-extent.js
+++ js/liveability/urban/change-in-urban-land-use-extent.js
@@ -62,11 +62,11 @@ print("<h3>Urban area growth between 1999 and current by region</h3>(Current dat
 var regions = dataHead.slice(1, dataHead.length - 2);
 
 
-print("<div class=subregions><ul id=regionCheckboxes>");
+print("<div class=subregions><ul id=regionCheckboxes>\n");
 regions.forEach(function (r, i) {
-	print(String.format("<li><input type=checkbox value=\"{0}\" id=checkbox_{0} {2} onchange=\"showHideChart(this)\" /><label for=checkbox_{0}>{1}</label>", r.toKebabCase(), r, i == 0 ? "checked" : ""));
+	print(String.format("<li><input type=checkbox value=\"{0}\" id=checkbox_{0} {2} onchange=\"showHideChart(this)\" /><label for=checkbox_{0}>{1}</label>\n", r.toKebabCase(), r, i == 0 ? "checked" : ""));
 });
-print("</ul>");
+print("</ul></div>\n");
 
 
 regions.forEach(function (region, i) {
