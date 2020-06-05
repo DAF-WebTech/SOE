7cd7985e8d8e9c17427a9f87a4f32a713e75b44d
David Porter <david.porter@daf.qld.gov.au>
2020-06-05 16:12:22 +1000

    rewrite page

--- js/liveability/rural/change-in-rural-land-use-extent.js
+++ js/liveability/rural/change-in-rural-land-use-extent.js
@@ -6,118 +6,98 @@ var results = Papa.parse(
 	csv,
 	{
 		skipEmptyLines: true,
-		dynamicTyping: true
+		dynamicTyping: true,
+		header: true
 	}
 );
 
-var dataHead = results.data.shift();
 var data = results.data;
+var latestYear = results.meta.fields.slice(-1)[0];
+var firstYear = results.meta.fields[3];
+
+// group by region an by category
+var regions = {};
+var categories = {}
+data.forEach(function(record) {
+	if (!regions[record.Region])
+		regions[record.Region] = [];
+	regions[record.Region].push(record);
+
+	if (record.Use != "total area mapped") {
+		if(!categories[record.Use])
+			categories[record.Use] = [];
+		categories[record.Use].push(record);
+	}
+});
+
 
 ///////////////////////////////////////////////////
+// 1. stacked column chart for qld figures
 var arrayTable = [["", "1999", "current"]];
-arrayTable.push([data[2][0], data[2][14], data[5][14]]);
-arrayTable.push([data[3][0], data[3][14], data[6][14]]);
-arrayTable.push([data[1][0], data[1][14], data[4][14]]);
 
+Object.keys(categories).forEach(function(category) {
+	var start = 0, current = 0;
+	var records = categories[category];
+	records.forEach(function(record) {
+		start += record[firstYear];
+		var i = results.meta.fields.length - 1;
+		while (record[results.meta.fields[i]] == null)
+			--i;
+			current += record[results.meta.fields[i]];
+	});
+	arrayTable.push([category, start, current]);
 
-var heading = "Queensland rural area growth between 1999 and Current (current data is composed of regional data sourced at different times)";
+});
+
+var heading = "Queensland rural area growth between 1999 and Current*";
 var index = 0;
 var region = "queensland";
 
 var htmlTable = tableToHtml(arrayTable, false)
-print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
+print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody, null, null, null, "current data is composed of regional data sourced at different times"));
 
-arrayTable[0][0] = "Year";
+arrayTable[0][0] = "Time";
 chart1 = arrayTable.transpose();
 var columnChartOptions = getDefaultColumnChartOptions();
 columnChartOptions.isStacked = true;
 chartData = [{ type: "column", options: columnChartOptions, data: chart1 }];
 
 
-
-
-/////////////////////////////////////////////////////////////////////////////
-var totalQueenslandRuralAreas = data[4][14] + data[5][14] + data[6][14];
-var totalQueenslandArea = data[0][14];
-var queenslandNonRuralArea = totalQueenslandArea - totalQueenslandRuralAreas;
-
-arrayTable = [["", "Non Rural", "Rural Land in Intensive Use", "Rural Land in Extensive Use", "Rural Land not Settled"]];
-arrayTable.push(["Hectares", queenslandNonRuralArea, data[4][14], data[5][14], data[6][14]]);
+//////////////////////////////////////////////////////////////////
+// 2. Pie chart
 heading = "Queensland rural area as a proportion of total state area";
 
-htmlTable = tableToHtml(arrayTable, false)
-print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
 
-arrayTable[0][0] = "Year";
-var chart2 = arrayTable.transpose();
 
-var pieChartOptions = getDefaultPieChartOptions();
-chartData.push({ type: "pie", options: pieChartOptions, data: chart2 });
+
+
+
+///////////////////////////////////////////////////////////////////
+// 3a. checkboxes for each region
+var checkboxes = "<div class=\"region-info region-queensland\"> \n";
+checkboxes += "  <ul id=regioncheckboxlist> \n";
+Object.keys(regions).forEach(function(regionName, i) {
+	var region = regions[regionName];
+	checkboxes += String.format("    <li><input type=checkbox id=checkbox_{0} value={0} {2} onclick=showHideChart><label for=checkbox_{0}>{1}</label> \n", regionName.toKebabCase(), regionName, (i == 0 ? "checked" : ""));
+});
+checkboxes += "  </ul>\n";
+checkboxes += "</div>";
 
 
 //////////////////////////////////////////////////////////////////////
-
-print("<h3>Rural area growth between 1999 and current by region (current data is composed of regional data sourced at different times).</h3>");
-
-var regions = dataHead.slice(2, dataHead.length - 1);
-
-print("<ul id=regionCheckboxes>");
-regions.forEach(function (r, i) {
-	print(String.format("<li><input type=checkbox value=\"{0}\" id=checkbox_{0} {2} onchange=\"showHideChart(this)\" /><label for=checkbox_{0}>{1}</label>", r.toKebabCase(), r, i == 0 ? "checked" : ""));
-});
-print("</ul>");
-
-
-regions.forEach(function (region, i) {
-
-	// this first one goes inside the queensland region, selectable by the checkboxes
-	arrayTable = [["", "1999", data[7][i + 2]]];
-	arrayTable.push([data[2][0], data[2][i + 2], data[5][i + 2]]);
-	arrayTable.push([data[3][0], data[3][i + 2], data[6][i + 2]]);
-	arrayTable.push([data[1][0], data[1][i + 2], data[4][i + 2]]);
-
-	heading = String.format("{0} urban area growth between 1999 and {1}", region, data[7][i + 2]);
-
-	htmlTable = tableToHtml(arrayTable, false);
-	var extraClass = "region-" + region.toKebabCase();
-	if (i > 0)
-		extraClass += " initial-hide";
-	print(String.format(regionInfoTemplate, "queensland", heading, index++, htmlTable.thead, htmlTable.tbody, "", extraClass));
-
-	arrayTable[0][0] = "Year";
-	arrayTable[0][2] = data[7][i + 1];
-	var myChart = arrayTable.transpose();
-	columnChartOptions = getDefaultColumnChartOptions();
-	columnChartOptions.vAxis.title = "Hectares";
-	columnChartOptions.vAxis.format = "short";
-	columnChartOptions.isStacked = true;
-	chartData.push({ type: "column", options: columnChartOptions, data: myChart });
-
-	// we also put it into its own div
-	print(String.format(regionInfoTemplate, region.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody));
-	chartData.push({ type: "column", options: columnChartOptions, data: myChart });
-
-	///////////////////////////////////////
-	//the rest go in each div for region
-	var regionNonRuralArea = data[0][i + 2] - (data[4][i + 2] + data[5][i + 2] + data[6][i + 2]);
-
-	arrayTable = [["", "Non Rural", "Rural Land in Intensive Use", "Rural Land in Extensive Use", "Rural Land not Settled"]];
-	arrayTable.push(["Hectares", regionNonRuralArea, data[4][i + 2], data[5][i + 2], data[6][i + 2]]);
-	heading = String.format("{0} Rural area as a proportion of total Region area in {1}", region, data[7][i + 2]);
-
-	htmlTable = tableToHtml(arrayTable, false)
-	print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
-
-	arrayTable[0][0] = "Year";
-	var chart2 = arrayTable.transpose();
-
-	var pieChartOptions = getDefaultPieChartOptions();
-	pieChartOptions.sliceVisibilityThreshold = 0;
-	chartData.push({ type: "pie", options: pieChartOptions, data: chart2 });
+// 3b.  these charts show/hide depending on region checkboxes
+heading = "Rural area growth between 1999 and current by region*";
 
 
 
-});
+
+
+
+"Current data is composed of regional data sourced at different times"
+
+
+
+
 
 
 
