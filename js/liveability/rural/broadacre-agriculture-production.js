7620135a70b774635949139108e55ac90b2c21f1
David Porter <david.porter@daf.qld.gov.au>
2020-06-09 12:33:45 +1000

    combine line charts into one

--- js/liveability/rural/broadacre-agriculture-production.js
+++ js/liveability/rural/broadacre-agriculture-production.js
@@ -1,13 +1,16 @@
+"use strict";
+
 if (typeof csv == "undefined") {
 	var csv = '%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%';
 }
 
 var headerIndex = 0;
 try {
-	var result = Papa.parse(csv, { header: true, 
-		dynamicTyping: true, 
+	var result = Papa.parse(csv, {
+		header: true,
+		dynamicTyping: true,
 		skipEmptyLines: true,
-		transformHeader: function(header) {
+		transformHeader: function (header) {
 			return headerIndex++ > 7 ? header + "v" : header; // handle our duplicate keys
 		}
 	});
@@ -17,21 +20,24 @@ try {
 
 // group by region
 var regions = {};
-result.data.forEach(function(record) {
+result.data.forEach(function (record) {
+
+	if (record.Product == "Total") return;
 
 	if (!regions[record.Region])
 		regions[record.Region] = [];
 
+	
 	regions[record.Region].push(record);
 
-	});
+});
 
 delete regions["Torres Strait NRM region"]; // no data for this one
 
 
 // get some keys
-var tonnesYears = result.meta.fields.slice(4, 7); 
-var valuesYears = result.meta.fields.slice(-3); 
+var tonnesYears = result.meta.fields.slice(4, 7);
+var valuesYears = result.meta.fields.slice(-3);
 
 // used by all iterations
 var chartData = [];
@@ -42,77 +48,84 @@ var index = 0;
 // find out what we are to do
 
 
-var drawCharts = function(record) { // a row from the data file
+var drawColumnCharts = function (record) { // a row from the data file
 
-			//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		// first charts are column chart for tonnes for each region for each product, including queensland as a region
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// first charts are column chart for tonnes for each region for each product, including queensland as a region
 
-		var heading = String.format("Production amount of {0} in {1}", record.Product, record.Region);
+	var heading = String.format("Production amount of {0} in {1}", record.Product, record.Region);
 
-		arrayTable = [["Year", "Tonnes"]];
-		var sum = 0;
-		var hasNP = false;  // if there's at least one np value, then we'll add the disclaimer
-		tonnesYears.forEach(function (key) {
-			sum += Number(record[key]);
-			arrayTable.push([key.replace("-", "–"), record[key]]); // replace with &ndash;
-			hasNP = hasNP ||  record[key] == "n.p.";
-		});
+	var arrayTable = [["Year", "Tonnes"]];
+	var sum = 0;
+	var hasNP = false;  // if there's at least one np value, then we'll add the disclaimer
+	tonnesYears.forEach(function (key) {
+		sum += Number(record[key]);
+		arrayTable.push([key.replace("-", "–"), record[key]]); // replace with &ndash;
+		hasNP = hasNP || record[key] == "n.p.";
+	});
 
-		htmlTable = tableToHtml(arrayTable, false);
-		if (sum == 0) {
-			print(String.format(regionInfoTemplateTableOnly, record.Region.toKebabCase(), heading, 9999, htmlTable.thead, htmlTable.tbody));
-		}
-		else {
-			print(String.format(regionInfoTemplate, record.Region.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody, 
-				null, null, null, 
-				(hasNP ? "NB In 2018–19 some hay crops were not reported at the NRM level due to confidentiality, therefore the sum of the regions does not equal the Queensland total for value and production.  This is indicated by n.p. (not published) " : "")));
-			chartData.push({ data: arrayTable, type: "column" });
-			var options = getDefaultColumnChartOptions();
-			options.vAxis.title = "Tonnes";
-			chartData[chartData.length - 1].options = options;
-		}
-
-		//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-		// second charts are line chart for dollars for each region for each product, including queensland as a region
-
-		heading = String.format("Production value of {0} in {1}", record.Product, record.Region);
-
-		arrayTable = [["Year", "Value"]];
-		sum = 0;
-		hasNP = false; // if there's at least one np value, then we'll add the disclaimer
-		valuesYears.forEach(function (key) {
-			sum += Number(record[key]);
-			arrayTable.push([key.replace("v", "").replace("-", "–"), record[key]]);
-			hasNP = hasNP ||  record[key] == "n.p.";
-		});
-
-		htmlTable = tableToHtml(arrayTable, false);
-		if (sum == 0) {
-			print(String.format(regionInfoTemplateTableOnly, record.Region.toKebabCase(), heading, 9999, htmlTable.thead, htmlTable.tbody));
-		}
-		else {
-			print(String.format(regionInfoTemplate, record.Region.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody, 
-				null, null, null, 
-				(hasNP ? "NB In 2018–19 some hay crops were not reported at the NRM level due to confidentiality, therefore the sum of the regions does not equal the Queensland total for value and production.  This is indicated by n.p. (not published) " : "")));
-			chartData.push({ data: arrayTable, type: "line" });
-			var options = getDefaultLineChartOptions();
-			options.vAxis.title = "Value ($)";
-			chartData[chartData.length - 1].options = options;
-		}
+	var htmlTable = tableToHtml(arrayTable, false);
+	if (sum == 0) {
+		print(String.format(regionInfoTemplateTableOnly, record.Region.toKebabCase(), heading, 9999, htmlTable.thead, htmlTable.tbody));
+	}
+	else {
+		print(String.format(regionInfoTemplate, record.Region.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody,
+			null, null, null,
+			(hasNP ? "NB In 2018–19 some hay crops were not reported at the NRM level due to confidentiality, therefore the sum of the regions does not equal the Queensland total for value and production.  This is indicated by n.p. (not published) " : "")));
+		chartData.push({ data: arrayTable, type: "column" });
+		var options = getDefaultColumnChartOptions();
+		options.vAxis.title = "Tonnes";
+		chartData[chartData.length - 1].options = options;
+	}
 
 }
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// second charts are line chart for dollars for each region for each product, including queensland as a region
+var drawLineChart = function (records, regionName) {
 
+	var heading = String.format("Production values in {0}", regionName);
 
-
-Object.keys(regions).forEach(function(regionName) {
-
-	regions[regionName].forEach(function(record) {
-		if (record.Product == "Total")
-			return;
-
-		drawCharts(record); // one row from the data file
-
+	var arrayTable = [["Product"]];
+	valuesYears.forEach(function(year) {
+		arrayTable[0].push(year.replace("-", "–").replace("v", ""));
 	});
+	records.forEach(function(record) {
+		var row = [record.Product];
+		valuesYears.forEach(function(year) {
+			row.push(record[year]);
+		});
+		arrayTable.push(row);
+	});
+
+	var htmlTable = tableToHtml(arrayTable, false);
+	
+	arrayTable = arrayTable.transpose();
+
+	var hasNP = false;
+	// if (sum == 0) {
+	// 	print(String.format(regionInfoTemplateTableOnly, regionName.toKebabCase(), heading, 9999, htmlTable.thead, htmlTable.tbody));
+	// }
+	// else {
+		print(String.format(regionInfoTemplate, regionName.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody,
+			null, null, null,
+			(hasNP ? "NB In 2018–19 some hay crops were not reported at the NRM level due to confidentiality, therefore the sum of the regions does not equal the Queensland total for value and production.  This is indicated by n.p. (not published) " : "")));
+		chartData.push({ data: arrayTable, type: "line" });
+		var options = getDefaultLineChartOptions();
+		options.vAxis.title = "Value ($)";
+		chartData[chartData.length - 1].options = options;
+//	}
+
+};
+
+
+
+Object.keys(regions).forEach(function (regionName) {
+
+	regions[regionName].forEach(function (record) {
+		drawColumnCharts(record); // one row from the data file
+	});
+
+	drawLineChart(regions[regionName], regionName); // array of lines from the data file
 
 });
 
