032337fc598dca981871282694a0d0f101763ad3
David Porter <david.porter@daf.qld.gov.au>
2020-06-11 13:10:15 +1000

    checkboxes for subcatchments

--- js/biodiversity/terrestrial/extent-rate-change-riparian-vegetation.js
+++ js/biodiversity/terrestrial/extent-rate-change-riparian-vegetation.js
@@ -1,3 +1,33 @@
+var makeChart = function(record, isSubcatchment) {
+	var heading = "Loss of woody vegetation";
+	var myheading = heading + " in " + record.Subcatchment;
+
+	var keys = result.meta.fields.slice(3, 5);
+	var arrayTable = [[""]];
+	keys.forEach(function(key) {
+		arrayTable[0].push(key.replace("_", "–")); // &endash;
+	});
+	arrayTable.push([record.Subcatchment, record[keys[0]], record[keys[1]]]);
+
+	var htmlTable = tableToHtml(arrayTable, false, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+	htmlTable.thead = htmlTable.thead.toLowerCase();
+	var extraClass = null;
+	if (isSubcatchment)
+		extraClass = "subregion-" + record.Subcatchment.toKebabCase();
+	print(String.format(regionInfoTemplate, 
+		record.Catchment.toKebabCase(), myheading , index++, htmlTable.thead, htmlTable.tbody, null, extraClass));
+	
+	arrayTable[0][0] == "Catchment";
+	var options = getDefaultLineChartOptions();
+	options.vAxis.title = "Percent (%)"
+	options.legend.position = "none";
+	for (var i = 0; i < arrayTable[0].length; ++i)
+		arrayTable[0][i] = arrayTable[0][i].split(" ")[0];
+	chartData.push({data: arrayTable.transpose(), options: options, type: "line" });
+
+}
+
+
 if (typeof csv == "undefined") {
 	var csv = '%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%';
 }
@@ -9,34 +39,44 @@ try {
 }
 
 var chartData = [];
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// chart 1 is qld., 
-var heading = "Loss of woody vegetation";
 var index = 0;
-result.data.forEach(function(record) {
-	
-		var myheading = heading + " in " + record.Subcatchment;
-		var keys = result.meta.fields.slice(3, 5);
-		var arrayTable = [["Catchment"]];
-		keys.forEach(function(key) {
-			arrayTable[0].push(key.replace("_", "–")); // &endash;
-		});
-		arrayTable.push([record.Subcatchment, record[keys[0]], record[keys[1]]]);
 
-		var htmlTable = tableToHtml(arrayTable, false, {minimumFractionDigits: 2, maximumFractionDigits: 2})
-		print(String.format(regionInfoTemplate, record.Catchment.toKebabCase(), myheading , index++, htmlTable.thead, htmlTable.tbody));
-		
-		var options = getDefaultLineChartOptions();
-		options.vAxis.title = "Percent (%)"
-		options.legend.position = "none";
-		for (var i = 0; i < arrayTable[0].length; ++i)
-			arrayTable[0][i] = arrayTable[0][i].split(" ")[0];
-		chartData.push({data: arrayTable.transpose(), options: options, type: "line" });
-
-	
-
-
+var catchments = result.data.filter(function(record) {
+	return record.Catchment == record.Subcatchment;
 });
+var subcatchments = result.data.filter(function(record) {
+	return record.Catchment != record.Subcatchment;
+});
+
+catchments.forEach(function(record) {
+
+		// make the charts for each catchment area
+		makeChart(record, false);
+
+		// make checkboxes
+		var div = String.format("<div class='region-info region-{0}'>\n", record.Catchment.toKebabCase());
+		div += "<h3>Choose a subcatchment</h3>\n";
+		div += "<ul style=columns:2 class=subcatchmentCheckboxList>\n";
+		subcatchments.forEach(function(subrecord) {
+			if (subrecord.Catchment == record.Catchment) {
+				div += String.format("<li><input id=subregion_{0} type=checkbox data-subregion={0}><label for=subregion_{0}>{1}</label>\n", 
+					subrecord.Subcatchment.toKebabCase(), subrecord.Subcatchment);
+			}
+		});
+		div += "</ul></div>";
+		print (div);
+		
+	
+});
+
+// iterate again, this time getting only the subcatchments
+result.data.forEach(function(record) {
+	if (record.Catchment != record.Subcatchment) {
+		// make the charts for each catchment area
+		makeChart(record, true);
+	}
+});
+
+
 
 print("<script id=chartData type=application/json>" + JSON.stringify(chartData) + "</" + "script>");
