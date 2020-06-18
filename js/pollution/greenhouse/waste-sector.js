c4ec604d76e37419bb6cf1b973008021c5defef6
David Porter <david.porter@daf.qld.gov.au>
2020-06-19 07:15:56 +1000

    new chart

--- js/pollution/greenhouse/waste-sector.js
+++ js/pollution/greenhouse/waste-sector.js
@@ -1,28 +1,126 @@
-var result = Papa.parse("%globals_asset_file_contents:1455662^replace:\r\n:\\n%", { header: true, dynamicTyping: true, skipEmptyLines: true});
-var yearKeys = result.meta.fields.slice(1);
-var html = [];
-html.push("<script id=jsonheadings type=application/json>");
-html.push(JSON.stringify(yearKeys));
-html.push("</" + "script>");
+if (typeof csv == "undefined") {
+	var csv = '%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%';
+}
 
-html.push("<script id=jsondata type=application/json>");
-var categories = [];
-result.data.forEach(function(record) {
-		 if (record.Category == "Total")
-				 return;
-		 var newCategory = {name: record.Category.trim(), values: {}};
-		 yearKeys.forEach(function(year) {
-				 newCategory.values[year] = record[year]; 
-		 });
-		 categories.push(newCategory);
+var results = Papa.parse(
+	csv,
+	{
+		skipEmptyLines: true,
+		dynamicTyping: true,
+		header: true
+	}
+);
+
+var latestYear = results.meta.fields[results.meta.fields.length - 1]
+
+var categoryData = results.data.slice(0, 4);
+
+var qldData = results.data[4];
+
+var stateData = results.data.slice(4);
+
+
+var index = 0
+var region = "queensland"
+
+
+///////////////////////////////////////////////////
+// pie 1
+var heading = "Proportion of waste emissions by state, " + latestYear
+
+var tableChartData = []
+stateData.forEach(function(record) {
+	tableChartData.push([record.Category, record[latestYear]])
+})
+
+tableChartData.sort(function (a, b) {
+	return a[1] < b[1] ? 1 : -1;
 });
-html.push(JSON.stringify(categories));
-html.push("</" + "script>");
 
-var totals = result.data[result.data.length-1];
-delete totals.Category;
-html.push("<script id=jsontotals type=application/json>");
-html.push(JSON.stringify(totals));
-html.push("</" + "script>");
+tableChartData.unshift(["State", "Emissions (million tonnes)"]);
 
-print (html.join("\n"));
+var htmlTable = tableToHtml(tableChartData, false, {minimumFractionDigits: 3, maximumFractionDigits: 3});
+print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
+
+
+var frontEndCharts = [{
+	data: tableChartData,
+	type: "pie",
+	options: getDefaultPieChartOptions(),
+}];
+
+
+
+///////////////////////////////////////////////////
+// pie 2
+
+heading = "Proportion of Queensland’s waste emissions by category, " + latestYear
+
+tableChartData = [];
+categoryData.forEach(function(record) {
+	tableChartData.push([record.Category, record[latestYear]])
+})
+
+
+tableChartData.sort(function (a, b) {
+	return a[1] < b[1] ? 1 : -1;
+});
+
+tableChartData.unshift(["Category", "Emissions (million tonnes)"]);
+
+htmlTable = tableToHtml(tableChartData, false, {minimumFractionDigits: 3, maximumFractionDigits: 3});
+print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
+
+
+frontEndCharts.push({
+	data: tableChartData,
+	type: "pie",
+	options: getDefaultPieChartOptions(),
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// area
+heading = "Trends in Queensland’s waste emissions, by category"
+
+tableChartData = [["Year"]];
+results.meta.fields.slice(2).forEach(function(year) {
+	tableChartData[0].push(year);
+})
+
+categoryData.forEach(function(record) {
+	var item = [record.Category]
+	results.meta.fields.slice(2).forEach(function(year) {
+		item.push(record[year]);
+	})
+	tableChartData.push(item)
+})
+
+htmlTable = tableToHtml(tableChartData.transpose(), false, {minimumFractionDigits: 3, maximumFractionDigits: 3});
+print(String.format(regionInfoTemplate, region, heading, index++, htmlTable.thead, htmlTable.tbody));
+
+
+options = getDefaultAreaChartOptions()
+options.vAxis.title = "Tonnes (millions)"
+
+frontEndCharts.push({
+	data: tableChartData.transpose(),
+	type: "area",
+	options: options,
+});
+
+//////////////////////////////////////////////
+// table only
+heading = "Queensland’s total waste emissions";
+
+var tableData = [["Year", "Emissions (million tonnes)"]];
+
+results.meta.fields.slice(2).forEach(function(year) {
+	tableData.push([year, qldData[year]]);
+})
+
+htmlTable = tableToHtml(tableData, false, {minimumFractionDigits: 3, maximumFractionDigits: 3});
+print(String.format(regionInfoTemplateTableOnly, region, heading, index++, htmlTable.thead, htmlTable.tbody));
+
+
+print("<script id=chartData type=application/json>" + JSON.stringify(frontEndCharts) + "</" + "script>");
