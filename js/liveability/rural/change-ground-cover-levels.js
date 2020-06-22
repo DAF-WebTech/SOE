6517afa92ac2c3e27449f40f0505008b1cbd0e27
David Porter <david.porter@daf.qld.gov.au>
2020-06-22 13:16:57 +1000

    new content

---
+++ js/liveability/rural/change-ground-cover-levels.js
@@ -0,0 +1,45 @@
+if (typeof csv == "undefined") {
+	var csv = '%frontend_asset_metadata_data-file^as_asset:asset_file_contents^replace:\r\n:\\n%'
+}
+
+var results = Papa.parse(
+	csv,
+	{
+		skipEmptyLines: true,
+		dynamicTyping: true,
+		header: true
+	}
+)
+
+var index = 0
+var heading = "Mean late dry season ground cover (%) in " 
+var chartData = []
+
+var years = results.meta.fields.slice(2, results.meta.fields.length - 1)
+
+results.data.forEach(function(record) {
+
+	if (record.Type == "Region") {
+		
+		var arrayTable = [["Year", "Groundcover (%)"]]
+
+		years.forEach(function(year) {
+			arrayTable.push([year, record[year]]);
+		})
+
+		var htmlTable = tableToHtml(arrayTable, false, {minimumFractionDigits: 1, maximumFractionDigits: 1})
+		print(String.format(regionInfoTemplate, record.Region.toKebabCase(), heading, index++, htmlTable.thead, htmlTable.tbody, null, null, null ,heading + record.Region));
+		
+		chartData.push({ type: "line", data: arrayTable })
+		var columnChartOptions = getDefaultLineChartOptions()
+		columnChartOptions.vAxis.title = "Groundcover (%)"
+		chartData[chartData.length - 1].options = columnChartOptions;
+		
+
+	}
+
+})
+
+
+
+print("<script id=chartData type=application/json>" + JSON.stringify(chartData) + "</" + "script>")
