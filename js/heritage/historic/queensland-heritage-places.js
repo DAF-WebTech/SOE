c133cd6260b70142a8d708acd31b4918a14eb6b1
David Porter <david.porter@daf.qld.gov.au>
2020-06-23 11:16:57 +1000

    vaxis format

--- js/heritage/historic/queensland-heritage-places.js
+++ js/heritage/historic/queensland-heritage-places.js
@@ -19,7 +19,7 @@ var endKeys = results.meta.fields.slice(-3)
 var options = getDefaultColumnChartOptions()
 options.legend.position = "none"
 options.vAxis.minValue = 0
-options.vAxis.format = "#"
+options.vAxis.format = "0"
 
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
