280a48b49cb5912a232f570c728393565969a0f3
David Porter <david.porter@daf.qld.gov.au>
2020-06-24 10:26:29 +1000

    fix heading

--- js/liveability/urban/change-in-urban-land-use-extent.js
+++ js/liveability/urban/change-in-urban-land-use-extent.js
@@ -89,7 +89,7 @@ regions.forEach(function (region, i) {
 	arrayTable = [["", "1999", "current"]];
 	arrayTable.push(["Hectares", data[1][i + 1], data[2][i + 1]]);
 
-	heading = String.format("{0} urban area growth between 1999 and {1}", region, data[3][i + 1]);
+	heading = String.format("Urban area growth between 1999 and {0} in {1}", data[3][i + 1], region);
 
 	htmlTable = tableToHtml(arrayTable, false);
 
