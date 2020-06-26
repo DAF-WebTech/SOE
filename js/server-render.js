fdbb9682bd2aeb9fda298d43646efd39aa0a129d
David Porter <david.porter@daf.qld.gov.au>
2020-06-26 16:05:15 +1000

    remove errant trailing comma

--- js/server-render.js
+++ js/server-render.js
@@ -437,7 +437,7 @@ String.format = function (format, args) {
 };
 
 
-var twelvechartColors = ["#4285f4", "#db4437", "#f4b400", "#0f9d58", "#ab47bc", "#00acc1", "#ff7043", "#9e9d24", "#5c6bc0", "#f06292", "#00796b", "#c2185b"],
+var twelveChartColors = ["#4285f4", "#db4437", "#f4b400", "#0f9d58", "#ab47bc", "#00acc1", "#ff7043", "#9e9d24", "#5c6bc0", "#f06292", "#00796b", "#c2185b"];
 
 
 var chartColours = ["#4285f4", "#db4437", "#f4b400", "#0f9d58", "#ab47bc", "#00acc1", "#ff7043"];
