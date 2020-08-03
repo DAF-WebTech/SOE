e197b27a48719cd5d519bdf0ca4757a1b6d0d9ed
David Porter <david.porter@fastmail.com>
2020-08-03 12:58:33 +1000

    change checkbox heading

--- js/biodiversity/freshwater/condition-of-riverine-ecosystem-health.js
+++ js/biodiversity/freshwater/condition-of-riverine-ecosystem-health.js
@@ -57,7 +57,7 @@ var getCheckboxes = function (catchment) {
 	var checkboxen = ""
 	Object.keys(catchment).forEach(function (key, i) {
 		if (i == 0) {
-			checkboxen += '<div class="checkbox-panel"><h3>Select sub-catchments</h3><ul class="checkbox-list">\n';
+			checkboxen += '<div class="checkbox-panel"><h3>Select indicators</h3><ul class="checkbox-list">\n';
 		}
 		checkboxen += String.format('<li><input type="checkbox" id="checkbox-{0}" data-subregion="{0}" class="checkbox-subregion" {2} /><label for="checkbox-{0}">{1}</label>\n',
 			key.toKebabCase(), key.replace(/-/, "&ndash;"), (i == 0 ? "checked" : ""));
